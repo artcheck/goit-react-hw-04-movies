017fc44b4621a402ce9db86af59010b36d683bc3
Artem Gardysh <artemgardysh@gmail.com>
2021-07-01 08:58:16 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, NavLink, Switch } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import MoviePage from "./pages/moviePage/MoviePage";
 import MovieDetailsPage from "./pages/movieDetailsPage/MovieDetailsPage";
-import NotFound from "./pages/notFound/NotFound";
 import routes from "./routes/routes";
 import css from "./App.module.css";
 
@@ -39,7 +38,6 @@ const App = () => {
         <Route exact path={routes.home} component={HomePage} />
         <Route path={routes.movieDetails} component={MovieDetailsPage} />
         <Route path={routes.moviePage} component={MoviePage} />
-        <Route component={NotFound} />
       </Switch>
     </>
   );
