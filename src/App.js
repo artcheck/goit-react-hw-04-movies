dc9ed8101b11c53d05218f99c67e4a9c586ce924
Artem Gardysh <artemgardysh@gmail.com>
2021-07-03 01:23:27 +0300

    lazyLoad  updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, Component } from "react";
 import { Route, NavLink, Switch } from "react-router-dom";
 import routes from "./routes/routes";
 import css from "./App.module.css";
@@ -9,41 +9,43 @@ const MovieDetailsPage = lazy(() =>
   import("./pages/movieDetailsPage/MovieDetailsPage")
 );
 
-const App = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <header className={css.Header}>
-        <nav>
-          <ul className={css.list}>
-            <li className={css.listItem}>
-              <NavLink
-                exact
-                to={routes.home}
-                className={css.link}
-                activeClassName={css.activeLink}
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className={css.listItem}>
-              <NavLink
-                to={routes.moviePage}
-                className={css.link}
-                activeClassName={css.activeLink}
-              >
-                Movies
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Switch>
-        <Route exact path={routes.home} component={HomePage} />
-        <Route path={routes.movieDetails} component={MovieDetailsPage} />
-        <Route path={routes.moviePage} component={MoviePage} />
-      </Switch>
-    </Suspense>
-  );
-};
+class App extends Component {
+  rebder() {
+    return (
+      <Suspense fallback={<div>Loading...</div>}>
+        <header className={css.Header}>
+          <nav>
+            <ul className={css.list}>
+              <li className={css.listItem}>
+                <NavLink
+                  exact
+                  to={routes.home}
+                  className={css.link}
+                  activeClassName={css.activeLink}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className={css.listItem}>
+                <NavLink
+                  to={routes.moviePage}
+                  className={css.link}
+                  activeClassName={css.activeLink}
+                >
+                  Movies
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          <Route exact path={routes.home} component={HomePage} />
+          <Route path={routes.movieDetails} component={MovieDetailsPage} />
+          <Route path={routes.moviePage} component={MoviePage} />
+        </Switch>
+      </Suspense>
+    );
+  }
+}
 
 export default App;
